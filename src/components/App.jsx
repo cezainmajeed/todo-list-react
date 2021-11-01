f9a85511a4ad53924cc74fc46611524097b73150
Cezain Majeed <63138007+cezainmajeed@users.noreply.github.com>
2021-11-01 16:34:14 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function App() {
+  const [inputText, setInputText] = React.useState("");
+  const [items, setItems] = React.useState([]);
+
+  function handleChange(event) {
+    const newInput = event.target.value;
+    setInputText(newInput);
+  }
+  function add() {
+    setItems((prev) => {
+      return [...prev, inputText];
+    });
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={add}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => {
+            return <li>{todoItem}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
